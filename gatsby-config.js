f5f9677a9c581e300c93e2d86e6e6907bf77b9a6
sa-webb <sawebb30@gmail.com>
2020-07-30 10:11:18 -0400

    edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Austin Webb`,
     author: {
       name: `sa-webb`,
-      summary: `Software developer specializing in web technologies.`,
+      summary: `Software engineer specializing in web technologies.`,
     },
     description: `My personal website built with Gatsby.js`,
     siteUrl: `https://sa-webb.dev`,
