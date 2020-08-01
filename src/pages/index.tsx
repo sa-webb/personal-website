fe9f1a29a9d4a6ec6a03d4345d452cf6f81bac80
sa-webb <sawebb30@gmail.com>
2020-07-31 23:45:31 -0400

    feat: breakpoint paddingTop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,7 +44,18 @@ const useStyles = makeStyles(theme => ({
     },
   },
   root: {
-    paddingTop: `3em`,
+    [theme.breakpoints.down('sm')]: {
+      paddingTop: theme.spacing(1),
+    },
+    [theme.breakpoints.up('md')]: {
+      paddingTop: theme.spacing(2),
+    },
+    [theme.breakpoints.up('lg')]: {
+      paddingTop: theme.spacing(4),
+    },
+    [theme.breakpoints.up('xl')]: {
+      paddingTop: theme.spacing(6),
+    },
   },
   card: {
     height: "100%",
