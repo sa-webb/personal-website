3d22ed815891dbf13982745ab482310c40e0d3e6
sa-webb <sawebb30@gmail.com>
2020-08-18 13:55:01 -0400

    temp

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,26 +1,11 @@
 import React from "react"
 
-import ParticlesBG from "../components/particles"
 import SEO from "../components/seo"
-
-import Container from "@material-ui/core/Container"
+import Blog from "../components/Blog/"
 import { makeStyles } from "@material-ui/styles"
-import Grid from "@material-ui/core/Grid"
-import { Typography } from "@material-ui/core"
-import Header from "../layout/Header"
 
 const useStyles = makeStyles({
-  root: {
-    paddingTop: "10em",
-  },
-  opac: {
-    height: "15vh",
-    backgroundColor: `rgba(0, 0, 0, 0.5)`,
-    color: `#fff`,
-    marginLeft: 'auto',
-    marginRight: 'auto'
-
-  },
+  
 })
 
 const BlogPage = () => {
@@ -28,25 +13,7 @@ const BlogPage = () => {
   return (
     <>
       <SEO title="Blog" />
-      <ParticlesBG />
-      <Header siteTitle='Austin Webb' />
-
-      <Container maxWidth="md" className={classes.root}>
-        <Grid
-          container
-          item
-          xs={12}
-          sm={12}
-          md={8}
-          direction="column"
-          alignItems="center"
-          alignContent="center"
-          justify="center"
-          className={classes.opac}
-        >
-          <Typography component="h1" variant="h3">Blogs coming soon!</Typography>
-        </Grid>
-      </Container>
+      <Blog />
     </>
   )
 }
