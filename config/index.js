9f9ef87440ea208f6722598c6ea3de31e005ec5a
sa-webb <sawebb30@gmail.com>
2020-08-19 15:11:09 -0400

    initial

---
+++ config/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: `Austin Webb`,
+  author: {
+    name: `sa-webb`,
+    summary: `Software engineer specializing in web technologies.`,
+  },
+  description: `My personal website built with Gatsby.js`,
+  siteUrl: `https://sa-webb.dev`,
+  social: {
+    spectrum: `austin-webb`,
+  },
+}
