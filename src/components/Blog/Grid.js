cd5f45c1281ef1fcc9b40f9d7ff1aa4aadb7a1d6
sa-webb <sawebb30@gmail.com>
2020-08-19 00:22:00 -0400

    temp

---
+++ src/components/Blog/Grid.js
@@ -0,0 +1,43 @@
+import React from "react"
+import Container from "@material-ui/core/Container"
+import { makeStyles } from "@material-ui/core/styles"
+import { Grid, CardActionArea, Card, Typography } from "@material-ui/core"
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    a: {
+      boxShadow: "none",
+    },
+  },
+  root: {
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: theme.spacing(1),
+    },
+    [theme.breakpoints.up("md")]: {
+      paddingTop: theme.spacing(2),
+    },
+    [theme.breakpoints.up("lg")]: {
+      paddingTop: theme.spacing(4),
+    },
+    [theme.breakpoints.up("xl")]: {
+      paddingTop: theme.spacing(6),
+    },
+  },
+}))
+
+const BlogGrid = ({ children }) => {
+  const classes = useStyles()
+  React.Children.map(children, Child => {
+    return (
+      <Container maxWidth="md" className={classes.root}>
+        <Grid container spacing={0}>
+          <Grid item xs={12} sm={6} md={6}>
+            {Child}
+          </Grid>
+        </Grid>
+      </Container>
+    )
+  })
+}
+
+export default BlogGrid
