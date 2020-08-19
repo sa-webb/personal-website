bc9ec7f78d0ae4dbb8f2f9c7dacc7c40a9ec606e
sa-webb <sawebb30@gmail.com>
2020-08-18 20:48:10 -0400

    initial

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,21 +1,76 @@
 import React from "react"
+import { Router, Link } from "@reach/router"
 
 import SEO from "../components/seo"
-import Blog from "../components/Blog/"
-import { makeStyles } from "@material-ui/styles"
 
-const useStyles = makeStyles({
-  
-})
+import Home from "../components/Blog/Home"
+import Header from "../components/Blog/Header"
 
-const BlogPage = () => {
+import { CssBaseline, Container, Toolbar, makeStyles } from "@material-ui/core"
+
+const sections = [
+  { title: "Contact", url: "blog/contact/" },
+  { title: "Category", url: "blog/category/" },
+  { title: "Culture", url: "#" },
+  { title: "Business", url: "#" },
+]
+
+const useStyles = makeStyles(theme => ({
+  toolbarSecondary: {
+    justifyContent: "space-between",
+    overflowX: "auto",
+  },
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0,
+  },
+}))
+
+const Contact = React.lazy(() => import("../components/Blog/Contact"))
+const About = React.lazy(() => import("../components/Blog/Category"))
+const LazyComponent = ({ Component, ...props }) => (
+  <React.Suspense fallback={"<p>Loading...</p>"}>
+    <Component {...props} />
+  </React.Suspense>
+)
+
+const BlogPage = props => {
   const classes = useStyles()
   return (
     <>
       <SEO title="Blog" />
-      <Blog />
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header title="Blog" sections={sections} />
+        <Toolbar
+          component="nav"
+          variant="dense"
+          className={classes.toolbarSecondary}
+        >
+          {sections.map(section => (
+            <Link
+              to={section.url}
+              style={{
+                fontSize: 18
+              }}
+              // color="inherit"
+              // noWrap
+              // key={section.title}
+              // variant="body2"
+              // href={section.url}
+              // className={classes.toolbarLink}
+            >
+              {section.title}
+            </Link>
+          ))}
+        </Toolbar>
+      </Container>
+      <Router>
+        <Home path="blog" />
+        <LazyComponent Component={Contact} path="blog/contact" />
+        <LazyComponent Component={About} path="blog/category" />
+      </Router>
     </>
   )
 }
-
 export default BlogPage
