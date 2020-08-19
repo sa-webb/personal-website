9a45c2d990e06cf9aaf65383345f27c0ee56441f
sa-webb <sawebb30@gmail.com>
2020-08-19 00:22:07 -0400

    temp

--- src/components/Blog/Category.js
+++ src/components/Blog/Category.js
@@ -1,8 +1,35 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import BlogCard from './Card'
+import BlogCard from "./Card"
+import Container from "@material-ui/core/Container"
+import { makeStyles } from "@material-ui/core/styles"
+import { Grid, CardActionArea, Card, Typography } from "@material-ui/core"
+// import BlogGrid from "./Grid"
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    a: {
+      boxShadow: "none",
+    },
+  },
+  root: {
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: theme.spacing(1),
+    },
+    [theme.breakpoints.up("md")]: {
+      paddingTop: theme.spacing(2),
+    },
+    [theme.breakpoints.up("lg")]: {
+      paddingTop: theme.spacing(4),
+    },
+    [theme.breakpoints.up("xl")]: {
+      paddingTop: theme.spacing(6),
+    },
+  },
+}))
 
 export default function Category() {
+  const classes = useStyles()
   return (
     <StaticQuery
       query={graphql`
@@ -26,11 +53,15 @@ export default function Category() {
         }
       `}
       render={data => (
-        <div>
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <BlogCard data={node}/>
-          ))}
-        </div>
+        <Container maxWidth="md" className={classes.root}>
+          <Grid container spacing={0}>
+            <Grid item xs={12} sm={6} md={6}>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <BlogCard data={node} />
+              ))}
+            </Grid>
+          </Grid>
+        </Container>
       )}
     />
   )
