02f6b2303eaa18e875118855880aa138de572083
sa-webb <sawebb30@gmail.com>
2020-08-18 23:41:53 -0400

    inital

---
+++ src/components/Blog/Card.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link } from "gatsby"
+import { makeStyles } from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+})
+
+const BlogCard = ({ data }) => {
+  const classes = useStyles()
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <Link to={`/blog${data.fields.slug}`}>{data.frontmatter.title}</Link>
+      </CardActionArea>
+    </Card>
+  )
+}
+
+export default BlogCard
