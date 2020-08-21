a4da3cb3c86ad5a56f13a417917da0754e8bb64f
sa-webb <sawebb30@gmail.com>
2020-08-20 20:33:24 -0400

    author meta

--- config/index.js
+++ config/index.js
@@ -2,6 +2,10 @@ module.exports = {
   title: `Austin Webb`,
   author: {
     name: `sa-webb`,
+    degree: `B.S. Computer Science`,
+    email: `stevenaustinwebb@gmail.com`,
+    number: `423-760-0010`,
+    profession: `Software Engineer`,
     summary: `Software engineer specializing in web technologies.`,
   },
   description: `My personal website built with Gatsby.js`,
