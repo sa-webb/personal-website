4e391c16935e283e12bcc65895412a1915abc9d7
sa-webb <sawebb30@gmail.com>
2020-08-20 20:58:35 -0400

    feat: query all Blogs

--- src/components/Blog/Home.js
+++ src/components/Blog/Home.js
@@ -1,11 +1,30 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
-export default function() {
+import BlogCard from "./Card"
+import BlogGrid from "./Grid"
+
+const BlogHome = () => {
   return (
-    <div>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <h3>Home</h3>
+  <StaticQuery
+    query={graphql`
+      query BlogHomeQuery {
+        allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+          ...CategoryFragment
+        }
+      }
+    `}
+    render={data => (
+      <div className="blogs">
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <BlogGrid>
+            <BlogCard key={node.id} data={node} />
+          </BlogGrid>
+        ))}
       </div>
-    </div>
+    )}
+  />
   )
 }
+
+export default BlogHome
