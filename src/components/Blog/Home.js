fa83cb14e2b3e2bfaa43ba7e9c5a0c7cc16bcd7b
sa-webb <sawebb30@gmail.com>
2020-08-24 18:22:02 -0400

    change other categories

--- src/components/Blog/Home.js
+++ src/components/Blog/Home.js
@@ -1,30 +1,53 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
+import Container from "@material-ui/core/Container"
+import Grid from "@material-ui/core/Grid"
+import { makeStyles } from "@material-ui/core/styles"
+
 import BlogCard from "./Card"
 import BlogGrid from "./Grid"
 
-const BlogHome = () => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+    justify: "center",
+    alignItems: "center",
+    alignContent: "center"
+  },
+}))
+
+const HomePosts = () => {
+  const classes = useStyles()
+
   return (
-  <StaticQuery
-    query={graphql`
-      query BlogHomeQuery {
-        allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
-          ...CategoryFragment
+    <StaticQuery
+      query={graphql`
+        query BlogHomeQuery {
+          allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+            ...CategoryFragment
+          }
         }
-      }
-    `}
-    render={data => (
-      <div className="blogs">
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <BlogGrid>
-            <BlogCard key={node.id} data={node} />
-          </BlogGrid>
-        ))}
-      </div>
-    )}
-  />
+      `}
+      render={data => (
+        <Container maxWidth="lg" className={classes.root}>
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            alignItems="center"
+            alignContent="center"
+            justify="center"
+          >
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <BlogCard key={node.id} data={node} />
+            ))}
+          </Grid>
+        </Container>
+      )}
+    />
   )
 }
 
-export default BlogHome
+export default HomePosts
