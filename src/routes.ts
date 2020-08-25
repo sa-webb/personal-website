735220580596806ddd62c871bf79c495ba04716e
sa-webb <sawebb30@gmail.com>
2020-08-25 19:47:36 -0400

    blog routes & categories

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+export const BLOG_ROUTES = {
+    HOME: "blog",
+    COMPUTING: "blog/computing",
+    TUTORIALS: "blog/tutorials",
+    HEURISTICS: "blog/heuristics"
+}
+
+export const categories = [
+    { title: "All Posts", url: "blog/" },
+    { title: "Computing", url: "blog/computing/" },
+    { title: "Tutorials", url: "blog/tutorials/" },
+    { title: "Heuristics", url: "blog/heuristics/" },
+  ]
